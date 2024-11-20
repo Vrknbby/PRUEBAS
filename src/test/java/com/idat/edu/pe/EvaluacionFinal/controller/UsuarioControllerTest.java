a4bea9fc5ca0dd7546941429ed473541c6fad6a3
AndreHC06 <sm62949039@idat.pe>
2024-11-20 01:14:15 -0500

    UsuarioControllerTest buscarporid

---
+++ src/test/java/com/idat/edu/pe/EvaluacionFinal/controller/UsuarioControllerTest.java
@@ -0,0 +1,52 @@
+package com.idat.edu.pe.EvaluacionFinal.controller;
+
+import com.idat.edu.pe.EvaluacionFinal.model.Usuario;
+import com.idat.edu.pe.EvaluacionFinal.service.UsuarioServicio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UsuarioControllerTest {
+
+    @Autowired
+    private UsuarioController usuarioController;
+
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+    Usuario usuario;
+
+    @BeforeEach
+    void setUp() {
+        usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Cristian Medina");
+        usuario.setEmail("mak_21_05@hotmail.com");
+        usuario.setPassword("123456");
+        usuario.setFechaNacimiento(LocalDate.parse("2000-08-22"));
+        usuario.setFondos(new BigDecimal("740.00"));
+        usuario.setRoles(new ArrayList<>());
+    }
+
+    @Test
+    void obtenerUsuarioPorId() {
+        Optional<Usuario> usuarioEncontrado = usuarioController.obtenerUsuarioPorId(usuario.getId());
+
+        // Comparar el usuario encontrado con el usuario creado
+        assertEquals(usuario.getId(), usuarioEncontrado.get().getId());
+        assertEquals(usuario.getNombre(), usuarioEncontrado.get().getNombre());
+        assertEquals(usuario.getEmail(), usuarioEncontrado.get().getEmail());
+    }
+
+}
